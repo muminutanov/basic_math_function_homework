70495ac45099e43e8fcdcd8a878fd34beb0c0e91
muminutanov <muminutanov20@gmail.com>
2023-02-26 16:28:43 +0500

    17

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,4 @@
+import math
 def main():
     '''return the value of pi
     
@@ -7,4 +8,6 @@ def main():
     Returns:
         float: the value of pi.
     '''
-    return
+    return(math.pi)
+x=main()
+print(x)
